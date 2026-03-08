039bcaf8c3c827a76fe90683254e972ecb20df1d
Anhnguyendz882 <138796997+Anhnguyendz882@users.noreply.github.com>
2026-03-08 20:21:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const express = require("express")
+const fs = require("fs")
+
 const app = express()
+app.use(express.json())
 
-app.get("/",(req,res)=>{
- res.send("Web running")
+const DB = "./keydb.json"
+
+function loadDB(){
+    return JSON.parse(fs.readFileSync(DB))
+}
+
+function saveDB(data){
+    fs.writeFileSync(DB, JSON.stringify(data,null,2))
+}
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html")
 })
 
-const PORT = process.env.PORT || 3000
+app.get("/check", (req,res)=>{
 
-app.listen(PORT,()=>{
- console.log("Server running")
+    const key = req.query.key
+    const name = req.query.name
+
+    if(!key || !name) return res.json({status:"error"})
+
+    const db = loadDB()
+
+    const found = db.keys.find(k=>k.key === key)
+
+    if(!found)
+        return res.json({status:"invalid"})
+
+    if(found.name !== name)
+        return res.json({status:"wrong_name"})
+
+    res.json({
+        status:"ok",
+        name:found.name
+    })
 })
 
-require("./bot.js")
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("API KEY SYSTEM RUNNING")
+})
